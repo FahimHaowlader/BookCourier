734711376cbc290b1d47299a11d14cd278841cda
Fahim083 <fahimhaowlader07@gmail.com>
2025-12-10 20:47:00 +0600

    frebase setup done

---
+++ src/Context/AuthContext.js
@@ -0,0 +1,78 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import auth from "../Firebase/Firebase";
+import React from "react";
+import {
+    onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  updateProfile,
+  signInWithPopup,
+  signOut,
+} from   "firebase/auth";
+
+
+const context = createContext();
+export const useAuth = () => useContext(context);
+
+const AuthContext = ({children}) => {
+ const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [adminLoading, setAdminLoading] = useState(true);
+
+  const createEmailUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const emailUserSignIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const userInfoUpdate = (info) => {
+    setLoading(true);
+    return updateProfile(auth.currentUser, info);
+  };
+
+  const googleUser = () => {
+    setLoading(true);
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider);
+  };
+
+  const userSignOut = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const userStatus = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        setLoading(false);
+      } else {
+        setUser(null);
+        setLoading(false);
+      }
+    });
+      return () => userStatus();
+  }, []);
+  const authInfo = {
+    user,
+    setUser,
+    loading,
+    setLoading,
+    adminLoading,
+    setAdminLoading,
+    createEmailUser,
+    emailUserSignIn,
+    userInfoUpdate,
+    googleUser,
+    userSignOut,
+  };
+   return (<context.Provider value={authInfo}>
+    {children}
+    </context.Provider>)
+}
+
+export default AuthContext
